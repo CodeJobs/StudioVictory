6d5b6ade3336c939054463a06d4e38036b4eab29
Carlos Santana <carlos.x.santana.-nd@disney.com>
2015-08-02 15:42:16 -0700

    Fixing identation

--- app.js
+++ app.js
@@ -49,33 +49,33 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 // Export application or start the server
 if (!!module.parent) {
-    module.exports = app;
+  module.exports = app;
 } else {
-    app.listen(3000);
+  app.listen(3000);
 }
